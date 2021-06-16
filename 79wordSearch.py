477225f0e3b2535689678cc39cf53ea680b0a1a3
Shaw S. Yu <46137960+AhoyKakkoii@users.noreply.github.com>
2021-06-16 18:14:09 -0400

    79

--- 79wordSearch.py
+++ 79wordSearch.py
@@ -3,30 +3,35 @@ class Solution:
         """
         dfs, fast, need understand
         """
-        l, rows, cols = len(word), len(board), len(board[0])
-
-        def dfs(x, y, d):  # d is the depth of recursion
-
-            if d == l:
+        def dfs(x, y, depth):
+            if depth == len(word):
                 return True
-            else:
-                if 0 <= x < cols and 0 <= y < rows and board[y][x] == word[d]:
-                    board[y][x], tmp = "", board[y][x]
-                    for dx, dy in ((-1, 0), (1, 0), (0, 1), (0, -1)):
-                        if dfs(x + dx, y + dy, d + 1):
-                            return True
-                    board[y][x] = tmp
-        counter, starts = Counter(word), []
-        for row in range(rows):
-            for col in range(cols):
-                counter[board[row][col]] -= 1
-                if board[row][col] == word[0]:
-                    starts.append((row, col))
+
+            if 0 <= x < len(board) and 0 <= y < len(board[0]) and board[x][y] == word[depth]:
+                board[x][y], tmp = "", board[x][y]
+                for dx, dy in ((-1, 0), (1, 0), (0, -1), (0, 1)):
+
+                    if dfs(x+dx, y+dy, depth+1):
+                        return True
+
+                board[x][y] = tmp
+
+        counter = Counter(word)
+        starts = []
+        for i in range(len(board)):
+            for j in range(len(board[i])):
+                counter[board[i][j]] -= 1
+                if board[i][j] == word[0]:
+                    starts.append((i, j))
+
         if max(counter.values()) > 0:
             return False
-        for row, col in starts:
-            if dfs(col, row, 0):
+
+        for i, j in starts:
+            if dfs(i, j, 0):
                 return True
+
+        return False
 
         """
         backtracking, slow, need optimize
@@ -46,20 +51,21 @@ class Solution:
 
 #         return False
 
+    def bk(self, board, word, visited, curr):
 
-#     def bk(self, board, word, visited, curr):
+        if len(word) < 1:
+            return True
 
-#         if len(word)<1: return True
+        i, j = curr
+        neighbors = [(i, j) for i, j in [(i, j+1), (i, j-1), (i-1, j), (i+1, j)] if i >=
+                     0 and j >= 0 and i < len(board) and j < len(board[0]) and (i, j) not in set(visited)]
 
-#         i, j = curr
-#         neighbors = [(i, j) for i, j in [(i, j+1), (i, j-1), (i-1, j), (i+1, j)] if i>=0 and j>=0 and i<len(board) and j<len(board[0]) and (i, j) not in set(visited)]
+        # print(curr, neighbors, word[0], visited)
 
-#         # print(curr, neighbors, word[0], visited)
+        for n_i, n_j in neighbors:
+            if board[n_i][n_j] == word[0]:
 
-#         for n_i, n_j in neighbors:
-#             if board[n_i][n_j] == word[0]:
+                if self.bk(board, word[1:], visited+[(n_i, n_j)], (n_i, n_j)):
+                    return True
 
-#                 if self.bk(board, word[1:], visited+[(n_i, n_j)], (n_i, n_j)):
-#                     return True
-
-#         return False
+        return False
