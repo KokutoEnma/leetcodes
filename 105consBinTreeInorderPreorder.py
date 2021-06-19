e921e3d2bb333e3f55d69e5fb8ff301acd750ddd
Shaw S. Yu <46137960+AhoyKakkoii@users.noreply.github.com>
2021-06-18 21:38:36 -0400

    105 better sol

--- 105consBinTreeInorderPreorder.py
+++ 105consBinTreeInorderPreorder.py
@@ -27,3 +27,17 @@ class Solution:
         pre_index = 0
 
         return build(0, len(inorder)-1)
+
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        def helper(bound=None):
+            if not inorder or inorder[0] == bound:
+                return None
+            root = TreeNode(preorder.pop(0))
+            root.left = helper(root.val)
+            inorder.pop(0)
+            root.right = helper(bound)
+            return root
+
+        return helper()
