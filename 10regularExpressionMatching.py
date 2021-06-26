9c515f283de402aa09f1b16debd4ee3ad6a83836
Shaw S. Yu <46137960+AhoyKakkoii@users.noreply.github.com>
2021-06-26 16:27:14 -0400

    10

--- 10regularExpressionMatching.py
+++ 10regularExpressionMatching.py
@@ -1,30 +1,54 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         """
+        topdown iterative
+        """
+        ns = len(s)
+        np = len(p)
+
+        dp = [[False]*(np+1) for _ in range(ns+1)]
+
+        dp[0][0] = True
+
+        for i in range(1, np+1):
+            if p[i-1] == '*':
+                dp[0][i] = dp[0][i-2]
+        for i in range(1, ns+1):
+            for j in range(1, np+1):
+                if p[j-1] == s[i-1] or p[j-1] == '.':
+                    dp[i][j] = dp[i-1][j-1]
+                elif p[j-1] == '*':
+                    dp[i][j] = dp[i][j-2]
+                    if p[j-2] == '.' or p[j-2] == s[i-1]:
+                        dp[i][j] = dp[i][j] or dp[i-1][j]
+
+        return dp[-1][-1]
+
+        """
         bottom up
         """
 
-        dp = [[False]*(len(p)+1) for _ in range(len(s)+1)]
+#         dp = [[False]*(len(p)+1) for _ in range(len(s)+1)]
 
-        # the last one s="" p="" is always true
+#         # the last one s="" p="" is always true
 
-        dp[-1][-1] = True
 
-        for i in range(len(s), -1, -1):
-            for j in range(len(p)-1, -1, -1):
-                first_match = i < len(s) and p[j] in {s[i], '.'}
+#         dp[-1][-1]=True
 
-                if j+1 < len(p) and p[j+1] == '*':
-                    dp[i][j] = dp[i][j+2] or first_match and dp[i+1][j]
-                else:
-                    dp[i][j] = first_match and dp[i+1][j+1]
 
-        return dp[0][0]
+#         for i in range(len(s), -1, -1):
+#             for j in range(len(p)-1, -1, -1):
+#                 first_match = i<len(s) and p[j] in {s[i], '.'}
+
+#                 if j+1<len(p) and p[j+1] == '*':
+#                     dp[i][j] = dp[i][j+2] or first_match and dp[i+1][j]
+#                 else:
+#                     dp[i][j] = first_match and dp[i+1][j+1]
+
+#         return dp[0][0]
 
         """
-        top down approach does not work in iterative implementation
-        because * need to consider 0 times or more times...
-        
+        top down 
         recursion topdown
         """
 
